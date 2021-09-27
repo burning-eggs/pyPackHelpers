2fb70cf6ac1c3af11eb2cec8b93a16b1b0efe14e
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-27 21:29:34 +0300

    Refactor

--- scripts/recipe_conflicts.py
+++ scripts/recipe_conflicts.py
@@ -1,10 +1,38 @@
-# Researches crafttweaker.log after /ct conflicts to show what's wrong.
+# Generates changelogs based on git repo.
 #
 # Credits to Krutoy242 @ github for making this in javascript
 # wake-code @ github ported this file to python.
-# Original link: https://github.com/Krutoy242/Enigmatica2Expert-Extended/blob/master/dev/automation/recipe_conflicts.js
+# Original link: https://github.com/Krutoy242/Enigmatica2Expert-Extended/blob/master/dev/automation/changelog.js
 
+import asyncio
 import colorama
+import click
 
-#debug_log = open("logs/debug.log", mode="r", encoding="utf8")
+colorama.init()
 
+ALL_CONFLICTS = []
+
+
+def no_conflicts():
+    click.echo(f"  {colorama.Fore.LIGHTBLUE_EX}.. No conflicts found!")
+
+
+async def init():
+    click.echo(f" {colorama.Fore.LIGHTBLUE_EX}.. Checking for recipe conflicts")
+
+    CRAFTTWEAKER_LOG = open("crafttweaker.log", encoding="utf-8")
+    CONFLICTING_START_TEXT = "[SERVER_STARTED][SERVER][INFO] Conflicting: \n"
+
+    for idx, line in enumerate(CRAFTTWEAKER_LOG, start=1):
+        if line in CONFLICTING_START_TEXT:
+            print("found conflicts")
+        elif line not in CONFLICTING_START_TEXT:
+            return no_conflicts()
+
+
+def main():
+    asyncio.run(init())
+
+
+if __name__ == "__main__":
+    main()
