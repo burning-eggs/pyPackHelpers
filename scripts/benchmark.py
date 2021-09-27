f8cbc67c2bb5b11d89328332e3d2019385374429
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-27 20:45:25 +0300

    Refactor

--- scripts/benchmark.py
+++
@@ -1,8 +0,0 @@
-# Collect information about load time for Debug.log
-# and output it in an .MD file.
-#
-# Credits to Krutoy242 @ github for making this in javascript
-# wake-code @ github ported this file to python.
-# Original link: https://github.com/Krutoy242/Enigmatica2Expert-Extended/blob/master/dev/automation/benchmark.js
-# Ported link:
-
---
+++ scripts/recipe_conflicts.py
@@ -0,0 +1,10 @@
+# Researches crafttweaker.log after /ct conflicts to show what's wrong.
+#
+# Credits to Krutoy242 @ github for making this in javascript
+# wake-code @ github ported this file to python.
+# Original link: https://github.com/Krutoy242/Enigmatica2Expert-Extended/blob/master/dev/automation/recipe_conflicts.js
+
+import colorama
+
+#debug_log = open("logs/debug.log", mode="r", encoding="utf8")
+
